64eedf60b3db5819092d173ce8a7aa7f0a83a885
RafaelSilva <rafaelsilvahome@gmail.com>
2025-10-25 17:17:33 -0300

    Parte  2 do trabalho

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,107 +2,128 @@
 
 #include <stdio.h>
 
-// Primeira carta
-    typedef struct Carta1 {
-        char estado1;
-        char codigo1[4];
-        char cidade1[50];
-        int populacao1;
-        int pontosTuristicos1;
-        float area1;
-        float pib1;
-    } carta1_t;
+    // Estrutura de Cartas
+    typedef struct Carta {
+        char estado;
+        char codigo[4];
+        char cidade[50];
+        int populacao;
+        int pontosTuristicos;
+        float area;
+        float pib;
+    } carta_t;
 
-    //Segunda carta
-    typedef struct Carta2 {
-        char estado2;
-        char codigo2[4];
-        char cidade2[50];
-        int populacao2;
-        int pontosTuristicos2;
-        float area2;
-        float pib2;
-    } carta2_t;
+
+    // Primeira e segunda cartas
+    carta_t c1;
+    carta_t c2;
+
+
+    //Funções para calcular Densidade Populacional;
+    float CalcDensidadePopulacional1(const carta_t *c1) {
+        float DensidadePopulacional1 = (c1->populacao / c1->area);
+        return DensidadePopulacional1;
+    }
+
+   float CalcDensidadePopulacional2(const carta_t *c2) {
+        float DensidadePopulacional2 = (c2->populacao / c2->area);
+        return DensidadePopulacional2;
+    }
+
+
+    //Funções para calcular PIB per capita;
+    float CalcPibPerCapita1(const carta_t *c1) {
+        float pibPerCapital1 = c1->pib / c1->populacao;
+        return pibPerCapital1;
+    }
+
+    float CalcPibPerCapita2(const carta_t *c2) {
+        float pibPerCapital2= c2->pib / c2->populacao;
+        return pibPerCapital2;
+    }
+
 
 int main() {
    
     //Inicialização da Primeira e Segunda Carta
-    carta1_t c1 = {
-        .estado1 = 'N', 
-        .codigo1 = "000",
-        .cidade1 = "Indefinida",
-        .populacao1 = 0,
-        .pontosTuristicos1 = 0,
-        .area1 = 0.0,
-        .pib1 = 0.0
+    carta_t c1 = {
+        .estado = 'N', 
+        .codigo = "000",
+        .cidade = "Indefinida",
+        .populacao = 0,
+        .pontosTuristicos = 0,
+        .area = 0.0,
+        .pib = 0.0
     };
 
-    carta2_t c2 = {
-        .estado2 = 'N',
-        .codigo2 = "000",
-        .cidade2 = "Indefinida",
-        .populacao2 = 0,
-        .pontosTuristicos2 = 0,
-        .area2 = 0.0,
-        .pib2 = 0.0
+    carta_t c2 = {
+        .estado = 'N',
+        .codigo = "000",
+        .cidade = "Indefinida",
+        .populacao = 0,
+        .pontosTuristicos = 0,
+        .area = 0.0,
+        .pib = 0.0
     };
-
-
 
     //Leitura da Primeira Carta
     printf("Cadastro da primeira carta:\n");
     printf("Estado (A-H): ");
-    scanf(" %c", &c1.estado1);
+    scanf(" %c", &c1.estado);
     printf("Código da Carta (ex: A01): ");
-    scanf("%s", c1.codigo1);
+    scanf("%s", c1.codigo);
     printf("Nome da Cidade: ");
-    scanf(" %[^\n]s", c1.cidade1);
+    scanf(" %[^\n]s", c1.cidade);
     printf("População: ");
-    scanf("%d", &c1.populacao1);
+    scanf("%d", &c1.populacao);
     printf("Área (em km²): ");
-    scanf("%f", &c1.area1);
+    scanf("%f", &c1.area);
     printf("PIB (em bilhões): ");
-    scanf("%f", &c1.pib1);
+    scanf("%f", &c1.pib);
     printf("Número de Pontos Turísticos: ");
-    scanf("%d", &c1.pontosTuristicos1);
+    scanf("%d", &c1.pontosTuristicos);
 
     // Leitura da Segunda Carta
     printf("\nCadastro da segunda carta :\n");
     printf("Estado (A-H): ");
-    scanf(" %c", &c2.estado2);
+    scanf(" %c", &c2.estado);
     printf("Código da Carta (ex: B02): ");
-    scanf("%s", c2.codigo2);
+    scanf("%s", c2.codigo);
     printf("Nome da Cidade: ");
-    scanf(" %[^\n]s", c2.cidade2);
+    scanf(" %[^\n]s", c2.cidade);
     printf("População: ");
-    scanf("%d", &c2.populacao2);
+    scanf("%d", &c2.populacao);
     printf("Área (em km²): ");
-    scanf("%f", &c2.area2);
+    scanf("%f", &c2.area);
     printf("PIB (em bilhões): ");
-    scanf("%f", &c2.pib2);
+    scanf("%f", &c2.pib);
     printf("Número de Pontos Turísticos: ");
-    scanf("%d", &c2.pontosTuristicos2);
+    scanf("%d", &c2.pontosTuristicos);
 
-
-    
     //Exibição da Primeira Carta
     printf("\nPrimeira Carta:\n");
-    printf("Estado: %c\n", c1.estado1);
-    printf("Código: %s\n", c1.codigo1);
-    printf("Nome da Cidade: %s\n", c1.cidade1);
-    printf("População: %d\n", c1.populacao1);
-    printf("Área: %.f km²\n", c1.area1);
-    printf("PIB: %.f bilhões de reais\n", c1.pib1);
-    printf("Número de Pontos Turísticos: %d\n", c1.pontosTuristicos1);
+    printf("Estado: %c\n", c1.estado);
+    printf("Código: %s\n", c1.codigo);
+    printf("Nome da Cidade: %s\n", c1.cidade);
+    printf("População: %d\n", c1.populacao);
+    printf("Área: %.f km²\n", c1.area);
+    printf("PIB: %.f bilhões de reais\n", c1.pib);
+    printf("Número de Pontos Turísticos: %d\n", c1.pontosTuristicos);
+    printf("Densidade Populacional: %.2f habitantes/km²\n",CalcDensidadePopulacional1(&c1));
+    printf("PIB per capita: %f reais\2n",CalcPibPerCapita1(&c1));
+
 
     // Exibição da Segunda Carta
-    printf("\nPrimeira Carta:\n");
-    printf("Estado: %c\n", c2.estado2);
-    printf("Código: %s\n", c2.codigo2);
-    printf("Nome da Cidade: %s\n", c2.cidade2);
-    printf("População: %d\n", c2.populacao2);
-    printf("Área: %.2f km²\n", c2.area2);
-    printf("Número de Pontos Turísticos: %d\n", c2.pontosTuristicos2);
+    printf("\nSegunda Carta:\n");
+    printf("Estado: %c\n", c2.estado);
+    printf("Código: %s\n", c2.codigo);
+    printf("Nome da Cidade: %s\n", c2.cidade);
+    printf("População: %d\n", c2.populacao);
+    printf("Área: %.2f km²\n", c2.area);
+    printf("Número de Pontos Turísticos: %d\n", c2.pontosTuristicos);
+    printf("Densidade Populacional: %.2f habitantes/km²\n", CalcDensidadePopulacional2(&c2));
+    printf("PIB per capita: %.2f reais\n", CalcPibPerCapita2(&c2));
 
     return 0;
+
 }
