4f30c8e3d523b888987963de947b7c53c0d9ac41
RafaelSilva <rafaelsilvahome@gmail.com>
2025-10-27 23:24:54 -0300

    Trabalho Finalizado

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,23 +1,29 @@
-
-/*
-TO DO;
-- Corrigir entradas inválidas (ex: letras em vez de números ou maiores do que aguentam os funções)
-- Melhorar a interface do usuário (ex: limpar a tela entre menus)
-- Função de entrada e saida de menus
-*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 
     /*Chamando funções que serão declaradas e descritas
-     posteriormente no codigo. Serão usadas principalmente em*/
-
+     posteriormente no codigo. Serão usadas principalmente entre menus*/
     void TerminaJogo();
     void MenuPrincipal();
+    void RetornaMenuInicioJogo();
 
+
+    //Função que limpa o buffer de input do usuário 
+    void LimpaDataBuffer() {
+        int num;
+        while ((num = getchar()) != '\n' && num != EOF);
+    }
+
+    //Limpa terminal(Windows 32 e Distros Linux)
+    static void LimpaTerminal() {
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
+    }
 
     // Estrutura de Cartas
     typedef struct Carta {
@@ -212,6 +218,7 @@ TO DO;
         CompDensidadePopulacional(&c1, &c2);
         CompPibPerCapita(&c1, &c2);
         CompSuperPoder(&c1, &c2); 
+        TerminaJogo();
     }
 
 
@@ -280,8 +287,8 @@ TO DO;
         }
         TerminaJogo();
     }
-    
-//Exibição da Primeira Carta
+
+    //Exibição da Primeira Carta
     void ExibeCarta1(carta_t c1) {
         printf("\nPrimeira Carta:\n");
         printf("Estado: %c\n", c1.estado);
@@ -309,19 +316,142 @@ TO DO;
         printf("PIB per capita: %f reais\n",CalcPibPerCapita(&c2));
     }
 
+    /*Os blocos de códigos entre a função NomeAtributo e MenuEscolhaDoisAtributos são relacionadas a opção 
+    "Escolher 2 atributos".*/
 
-    static void LimpaDataBuffer() {
-        int num;
-        while ((num = getchar()) != '\n' && num != EOF);
+    //Pointer que facilita o acesso aos nomes dos atributos dentro do struct Carta
+    static const char *NomeAtributo(int escolha) {
+        while(1) {
+            switch (escolha) {
+                case 1: return "População"; break;
+                case 2: return "Área"; break;
+                case 3: return "PIB"; break;
+                case 4: return "Pontos Turísticos"; break;
+                case 5: return "Densidade Populacional"; break;
+                case 6: return "PIB per Capita"; break;
+                case 7: return "Super Poder"; break;
+                default: return "Atributo Inválido"; break;
+            }
+        }
     }
 
-    static void LimpaTerminal() {
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif
+    //Função que inicializa os valores de atributos compostos e facilita a escolha do valor do atributo
+    static double ValorAtributo(carta_t *c, int escolha) {
+        if (!escolha){
+            return 0.0;
+        }
+        CalcDensidadePopulacional(c);
+        CalcPibPerCapita(c);
+        CalcSuperPoder(c);
+        switch (escolha) {
+            case 1: return c->populacao;
+            case 2: return c->area;
+            case 3: return c->pib;
+            case 4: return c->pontosTuristicos;
+            case 5: return c->densidadePopulacional;
+            case 6: return c->pibPerCapita;
+            case 7: return c->superPoder;
+            default: return 0.0;
+        }
     }
+
+    /*Função chamada por "MenuEscolhaDoisAtributos". É responsavel por exibir a escolha do segundo atributo,
+    realizar a soma e exibir os resultados*/
+    void EscolhaAninhadaAtributo(carta_t c1, carta_t c2, int primeiro) {
+       int segundo = 0;
+        while (1) {
+            printf("\nEscolha o segundo atributo (diferente de %d - %s):\n", primeiro, NomeAtributo(primeiro));
+            for (int i = 1; i <= 7; ++i) {
+                if (i == primeiro) continue;
+                printf("%d. %s\n", i, NomeAtributo(i));
+            }
+            printf("\nSegundo atributo: ");
+            if (scanf("%d", &segundo) != 1) {
+                LimpaDataBuffer();
+                printf("Entrada inválida. Tente novamente.\n");
+                continue;
+            }
+            if (segundo < 1 || segundo > 7 || segundo == primeiro) {
+                printf("Opção inválida. Escolha um atributo entre 1 e 7 diferente do primeiro.\n");
+                continue;
+            }
+            break;
+        }
+
+        //calcula valores dos atributos e passa copias para funções/blocos de códigos posteriores (variáveis double para evitar erros de precisão)
+        float v1_p = ValorAtributo(&c1, primeiro);
+        float v2_p = ValorAtributo(&c2, primeiro);
+        float v1_s = ValorAtributo(&c1, segundo);
+        float v2_s = ValorAtributo(&c2, segundo);
+
+        printf("\n--- Resultado da Comparação ---\n");
+        printf("%s: Carta1 = %.6g ; Carta2 = %.6g\n", NomeAtributo(primeiro), v1_p, v2_p);
+        printf("%s: Carta1 = %.6g ; Carta2 = %.6g\n", NomeAtributo(segundo), v1_s, v2_s);
+
+        //Definindo vencedor por atributo
+        if (primeiro == 5) {
+            if (v1_p < v2_p) printf("Vencedor no atributo 1 (%s): Carta 1\n", NomeAtributo(primeiro));
+            else if (v1_p > v2_p) printf("Vencedor no atributo 1 (%s): Carta 2\n", NomeAtributo(primeiro));
+            else printf("Atributo 1 (%s): Empate!\n", NomeAtributo(primeiro));
+        } else {
+            if (v1_p > v2_p) printf("Vencedor no atributo 1 (%s): Carta 1\n", NomeAtributo(primeiro));
+            else if (v1_p < v2_p) printf("Vencedor no atributo 1 (%s): Carta 2\n", NomeAtributo(primeiro));
+            else printf("Atributo 1 (%s): Empate!\n", NomeAtributo(primeiro));
+        }
+
+        if (segundo == 5) {
+            if (v1_s < v2_s) printf("Vencedor no atributo 2 (%s): Carta 1\n",NomeAtributo(segundo));
+            else if (v1_s > v2_s) printf("Vencedor no atributo 2 (%s): Carta 2\n", NomeAtributo(segundo));
+            else printf("Atributo 2 (%s): Empate!\n", NomeAtributo(segundo));
+        } else {
+            if (v1_s > v2_s) printf("Vencedor no atributo 2 (%s): Carta 1\n", NomeAtributo(segundo));
+            else if (v1_s < v2_s) printf("Vencedor no atributo 2 (%s): Carta 2\n", NomeAtributo(segundo));
+            else printf("Atributo 2 (%s): Empate!\n", NomeAtributo(segundo));
+        }
+
+        //Soma e decisão final
+        float soma1 = v1_p + v1_s;
+        float soma2 = v2_p + v2_s;
+        printf("\nSoma dos atributos: Carta 1 = %.6g ; Carta 2 = %.6g\n", soma1, soma2);
+        if (soma1 > soma2) {
+            printf("\nVencedor da rodada: Carta 1\n");
+        } else if (soma1 < soma2) {
+            printf("\nVencedor da rodada: Carta 2\n");
+        } else {
+            printf("\nEmpate!\n");
+        }
+
+        TerminaJogo();
+    }
+
+    /* menu dinâmico: escolhe primeiro atributo e chama a função de atribuição aninhada de atributo */
+    void MenuEscolhaDoisAtributos(carta_t c1, carta_t c2) {
+        int primeiro = 0;
+        while (1) {
+            printf("\nEscolha o primeiro atributo (1-7):\n");
+            printf("1. População");
+            printf("\n2. Área");
+            printf("\n3. PIB");
+            printf("\n4. Número de Pontos Turísticos");
+            printf("\n5. Densidade Populacional");
+            printf("\n6. PIB per capita");
+            printf("\n7. Superpoder\n");
+            printf("Primeiro atributo: ");
+            if (scanf("%d", &primeiro) != 1) {
+                LimpaDataBuffer();
+                printf("Entrada inválida. Tente novamente.\n");
+                continue;
+            }
+            if (primeiro < 1 || primeiro > 7) {
+                printf("Opção inválida. Digite um número entre 1 e 7.\n");
+                continue;
+            }
+            break;
+        }
+        EscolhaAninhadaAtributo(c1, c2, primeiro);
+    }
+    /*Fim do bloco de código relacionado a opção "Escolha 2"*/
+
 
     //Função para exibir as regras do jogo
     void MenuRegrasJogo() {
@@ -329,54 +459,19 @@ TO DO;
         while(1) {
             printf("\nRegras do Jogo Super Trunfo:\n");
             printf("1. Cada jogador possui uma carta com atributos diferentes.\n");
-            printf("2. Os jogadores escolhem um atributo para comparar.\n");
+            printf("2. Os jogadores escolhem um ou dois atributos para comparar.\n");
             printf("3. O jogador com o maior valor no atributo escolhido vence a rodada.\n");
             printf("4. Em caso de empate, nenhuma carta vence.\n");
             printf("5. O jogo pode ser jogado em várias rodadas, acumulando pontos.\n");
             printf("6. Divirta-se e boa sorte!\n");
-            printf("\n Voltar para o menu?(s/n)\n");
-            scanf(" %c", &escolha_f);
-            if (escolha_f == 'S' || escolha_f == 's' ) {
-                LimpaTerminal();
-                MenuPrincipal();
-                break;
-            } else if (escolha_f == 'N' || escolha_f =='n') {
-                exit(0);
-                break;
-            } else {
-                LimpaDataBuffer();
-                printf("\nOpção invalida. Tente novamente\n");
-                continue;
-            }
+            printf("\nObs: No caso do modo de jogo 'Escolha dois atributos', a soma dos atributos escolhidos é o que define o vencedor da partida\n.");
+            RetornaMenuInicioJogo();
         }
-     }
+    }
 
-  void MenuExibeTodasCartas(carta_t c1, carta_t c2) {
-        char escolha_f;
-        while (1) {
-            ExibeCarta1(c1);
-            ExibeCarta2(c2);
-            printf("\n Voltar para o menu?(s/n)\n");
-            scanf(" %c", &escolha_f);
-            if (escolha_f == 'S' || escolha_f == 's' ) {
-                LimpaTerminal();
-                MenuPrincipal();
-                break;
-            } else if (escolha_f == 'N' || escolha_f =='n') {
-                exit(0);
-                break;
-            } else {
-                LimpaDataBuffer();
-                printf("\nOpção invalida. Tente novamente\n");
-                continue;
-            }
-        }
-     }
-
-
-    //Função para exibir o menu de escolha de atributo -> Á fazer
+    //Função para exibir o menu de escolha de atributo
     void MenuEscolhaUmAtributo(const carta_t c1, const carta_t c2) {
-        int escolha = 0;
+        int escolha_f = 0;
         while (1) {
             printf("\nEscolha o atributo para comparação:\n");
             printf("1. População\n");
@@ -387,18 +482,19 @@ TO DO;
             printf("6. PIB per capita\n");
             printf("7. Superpoder\n");
             printf("Digite o número correspondente ao atributo escolhido: ");
-            scanf("%d", &escolha);
-            if (escolha < 1 || escolha > 7) {
+            scanf("%d", &escolha_f);
+            if (escolha_f < 1 || escolha_f > 7) {
                 printf("Opção inválida. Tente novamente.\n");
                 LimpaDataBuffer();
                 continue;
             }
-            if (escolha >= 1 && escolha <= 7) {
-                CompAtributoEscolhido(c1, c2, escolha);
+            if (escolha_f >= 1 && escolha_f <= 7) {
+                CompAtributoEscolhido(c1, c2, escolha_f);
             }
         break;
         }
     }
+
 
     //Menu de inicio de jogo. Usuario escolhe entre atributos aleatorios ou escolhidos
     void MenuInicioJogo() {
@@ -406,38 +502,69 @@ TO DO;
         while(1){
             printf("\nEscolha o modo de jogo:\n");
             printf("1. Escolha 1 Atributo\n");
-            printf("2. Escolha 2 Atributos");
+            printf("2. Escolha 2 Atributos\n");
             printf("3. Atributo Aleatório\n");
             printf("4. Comparar Todos os Atributos\n");
+            printf("5. Voltar para o menu principal");
             printf("5. Exibir Cartas\n");
+            printf("6. Voltar para o menu principal")
             scanf("%d", &modo);
-            if (modo < 1 || modo > 5) {
-                printf("Entrada inválida. Por favor, insira um número.\n");
+            if (modo < 1 || modo > 6) {
+                printf("Entrada inválida. Tente novamente\n");
                 LimpaDataBuffer();
                 continue;
             }
-            if (modo >= 1 && modo <= 4) {
+            if (modo >= 1 && modo <= 6) {
                 switch (modo) {
                     case 1:
                         MenuEscolhaUmAtributo(c1, c2);
                         break;
                     case 2:
-                        CompAtributoAleatorio(c1, c2);
+                        MenuEscolhaDoisAtributos(c1, c2);
                         break;
                     case 3:
-                        CompTodosAtributos(c1, c2);
+                        CompAtributoAleatorio(c1, c2);
                         break;
                     case 4:
-                        MenuExibeTodasCartas(c1, c2);
+                        CompTodosAtributos(c1, c2);
                         break;
+                    case 5:
+                        ExibeCarta1(c1);
+                        ExibeCarta2(c2);
+                        RetornaMenuInicioJogo();
+                        break;
+                    case 6:
+                        LimpaTerminal();
+                        MenuPrincipal();
                     default:
                         printf("Opção inválida. Tente novamente.\n");
                     continue;
                 }
-            } break;
+            }
         
         }
     }
+
+    void RetornaMenuInicioJogo() {
+        char escolha_f;
+        while (1) {
+            printf("\n Voltar para o menu?(s/n)\n");
+            scanf(" %c", &escolha_f);
+            if (escolha_f == 'S' || escolha_f == 's' ) {
+                LimpaTerminal();
+                MenuPrincipal();
+                break;
+            } else if (escolha_f == 'N' || escolha_f =='n') {
+                exit(0);
+                break;
+            } else {
+                LimpaDataBuffer();
+                printf("\nOpção invalida. Tente novamente\n");
+                continue;
+            }
+        }
+    }
+
 
     //Menu Principal (Inicio, Regras e Sair)
     void MenuPrincipal() {
@@ -449,7 +576,7 @@ TO DO;
             printf("3. Sair\n");
             printf("Escolha uma opção: \n");
             scanf("%d", &opcao);
-                if (opcao < 1 || opcao > 3) {
+                if (opcao < 1 || opcao > 4) {
                     printf("Entrada inválida. Tente novamente\n");
                     LimpaDataBuffer();
                     continue;
@@ -493,7 +620,6 @@ TO DO;
     }
 
 
-
 int main() {
    
     /*Inicializando a função "rand" (Retorna um número pseudoaleatório)
@@ -501,7 +627,8 @@ int main() {
     srand(time(0));
     rand();
 
-    //Leitura da Primeira Carta
+
+    //Cadastro da primeira carta: Lê o input do usuário e usa-o para definir os valores do struct carta_t c1
     printf("Cadastro da primeira carta:\n");
     printf("Estado (A-H): ");
     scanf(" %c", &c1.estado);
@@ -513,12 +640,12 @@ int main() {
     scanf(" %lu", &c1.populacao);
     printf("Área (em km²): ");
     scanf(" %f", &c1.area);
-    printf("PIB (em bilhões): ");
+    printf("PIB (em reais): ");
     scanf(" %f", &c1.pib);
     printf("Número de Pontos Turísticos: ");
     scanf(" %d", &c1.pontosTuristicos);
 
-    //Leitura da Segunda Carta
+     //Cadastro da segunda carta: Lê o input do usuário e usa-o para definir os valores do struct carta_t c2
     printf("\nCadastro da segunda carta :\n");
     printf("Estado (A-H): ");
     scanf(" %c", &c2.estado);
@@ -530,7 +657,7 @@ int main() {
     scanf(" %lu", &c2.populacao);
     printf("Área (em km²): ");
     scanf(" %f", &c2.area);
-    printf("PIB (em bilhões): ");
+    printf("PIB (em reais): ");
     scanf(" %f", &c2.pib);
     printf("Número de Pontos Turísticos: ");
     scanf(" %d", &c2.pontosTuristicos);
@@ -538,7 +665,9 @@ int main() {
     //Menu Principal: Exibe a opção de iniciar o jogo, ver as regras ou sair
     MenuPrincipal();
 
-    TerminaJogo();
+    while (1) {
+        TerminaJogo();
+    }
 
     return 0;
 }
