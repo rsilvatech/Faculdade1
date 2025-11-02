8101e3c4540f4fd1eb98de5fc48fabd284b69f79
RafaelSilva <rafaelsilvahome@gmail.com>
2025-11-02 17:38:13 -0300

    Trabalho Finalizado

--- Batalha_Naval.c
+++ Batalha_Naval.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TamTabuleiro 10
+#define TamHabilidade 5
+#define Habilidade 1
 #define TamNavio 3
-#define Agua 0
 #define Navio 3
+#define Agua 0
 
-int main() {
-
-    // ===== Bloco de definição e inicialização do tabuleiro
-
-    int tabuleiro[TamTabuleiro][TamTabuleiro];
-    for (int i = 0; i < TamTabuleiro; i++) {
-        for (int j = 0; j < TamTabuleiro; j++) {
-            tabuleiro[i][j] = Agua; //preenche todas as posições do tabulero com zeros
-        }
-    }
-
-    // ===== Bloco das coordenadas iniciais dos Navios =====
+// ===== Bloco das coordenadas iniciais dos Navios ===== //
 
     //Coordenadas iniciais do navio horizontal C(1,2)
     int linhaNavioHorizontal = 0;
@@ -34,8 +26,83 @@ int main() {
     int linhaNavioDiagonal2 = 6;
     int colunaNavioDiagonal2 = 3;
 
+// ===== Bloco de definição da Habilidade ===== //
 
-    // ===== Bloco de descrição das posições dos navios no tabuleiro =====
+// Função para construir o cone dentro de uma submatriz
+void ConstruirCone(int matrizCone[TamHabilidade][TamHabilidade]) {
+    for (int i = 0; i < TamHabilidade-2; i++) {
+        for (int j=0; j<TamHabilidade; j++) {
+            if (j>=TamHabilidade/2 - i && j <= TamHabilidade / 2 + i) {
+                matrizCone[i][j] = 1;
+            } else {
+                matrizCone[i][j] = 0;
+            }
+        }
+    }
+}
+
+// Função para construir a cruz dentro de uma submatriz
+void ConstruirCruz(int matrizCruz[TamHabilidade][TamHabilidade]) {
+    for (int i = 0; i < TamHabilidade; i++) {
+        for (int j=0; j<TamHabilidade; j++) {
+            if (i==TamHabilidade/2  || j == TamHabilidade / 2) {
+                matrizCruz[i][j] = 1;
+            } else {
+                matrizCruz[i][j] = 0;
+            }
+        }
+    }
+}
+
+// Função para construir o octaedro (lozango) dentro de uma submatriz
+void construirOctaedro(int matrizOctaedro[TamHabilidade][TamHabilidade]) {
+    for (int i = 0; i < TamHabilidade; i++) {
+        for (int j = 0; j < TamHabilidade; j++) {
+            int distancia = abs(i - TamHabilidade / 2) + abs(j - TamHabilidade / 2);
+            if (distancia <= TamHabilidade / 2) {
+                matrizOctaedro[i][j] = 1;
+            } else {
+                matrizOctaedro[i][j] = 0;
+            }
+        }
+    }
+}
+
+
+// Função para aplicar a matriz de habilidade ao tabuleiro
+void aplicarHabilidade(int tabuleiro[TamTabuleiro][TamTabuleiro], int submatriz[TamHabilidade][TamHabilidade], int origemLinha, int origemColuna) {
+    for (int i = 0; i < TamHabilidade; i++) {
+        for (int j = 0; j < TamHabilidade; j++) {
+            int linhaTab = origemLinha - TamHabilidade / 2 + i;
+            int colunaTab = origemColuna - TamHabilidade / 2 + j;
+
+            if (linhaTab >= 0 && linhaTab < TamTabuleiro && colunaTab >= 0 && colunaTab < TamTabuleiro) {
+                if (submatriz[i][j] == 1) {
+                    tabuleiro[linhaTab][colunaTab] = Habilidade;
+                }
+            }
+        }
+    }
+}
+
+
+int main() {
+
+    // ===== Bloco de definição e inicialização do tabuleiro e matrizes de habilidade ===== //
+
+    int tabuleiro[TamTabuleiro][TamTabuleiro];
+    int matrizCone[TamHabilidade][TamHabilidade];
+    int matrizCruz[TamHabilidade][TamHabilidade];
+    int matrizOctaedro[TamHabilidade][TamHabilidade];
+    
+
+    for (int i = 0; i < TamTabuleiro; i++) {
+        for (int j = 0; j < TamTabuleiro; j++) {
+            tabuleiro[i][j] = Agua; //preenche todas as posições do tabulero com zeros
+        }
+    }
+
+    // ===== Bloco de descrição das posições dos navios no tabuleiro ===== //
 
     // Verifica se o navio vertical cabe no tabuleiro
     if (linhaNavioHorizontal + TamNavio <= TamTabuleiro) {
@@ -139,8 +206,21 @@ int main() {
         return 1;
     }
 
-    // ===== Bloco de exibição do tabuleiro e das peças
+    // ===== Bloco de exibição do tabuleiro e das peças ===== //
 
+    
+    //Constrói e aplica as habilidades
+    ConstruirCone(matrizCone);
+    aplicarHabilidade(tabuleiro, matrizCone, 2, 7);
+\
+    construirOctaedro(matrizOctaedro);
+    aplicarHabilidade(tabuleiro, matrizOctaedro, 5, 2);
+
+    ConstruirCruz(matrizCruz);
+    aplicarHabilidade(tabuleiro, matrizCruz, 7, 7);
+
+
+    // ===== Bloco de exibição do tabuleiro e das peças ===== //
     char alfabeto[10] = {'A','B','C','D','E','F','G','H','I','J'};
 
     //Exibe tabuleiro e peças
